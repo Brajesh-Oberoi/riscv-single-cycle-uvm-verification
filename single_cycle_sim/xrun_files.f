dd557851d229815d9be79d1ca7abc07d71bf2bf3
brajesh <brajeshoberoi@gmail.com>
2026-06-18 21:41:40 +0530

    adding sim dir

---
+++ single_cycle_sim/xrun_files.f
@@ -0,0 +1,9 @@
+-uvm
+-access +rwc
+-incdir .
+-incdir ../single_cycle/imem_vip/
+-incdir ../single_cycle/
+-top top
+../single_cycle/imem_vip/imem_pkg.sv
+../single_cycle/imem_vip/imem_if.sv
+top.sv
